b984ae64049874073f7ecdd906a8c2acfc61795e
Doom <lightfollower1985@gmail.com>
2020-09-08 16:41:21 +0500

    common

---
+++ common/src/main/java/com/flamexander/cloud/common/dto/ProductDto.java
@@ -0,0 +1,11 @@
+package com.flamexander.cloud.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ProductDto {
+    Long Id;
+    String title;
+}
